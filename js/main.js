ba0ae79596064063241a7915b9d2006192c651ed
Lucía Wainfeld <luwainfeld@gmail.com>
2018-06-11 11:26:24 -0300

    corrijo merge

--- js/main.js
+++ js/main.js
@@ -1,132 +1,61 @@
-//En caso de emergencia, limpiar localStorage:
-//localStorage.clear()
-
-var paisesArray = paisesJSON.paises;
-
-var preguntas = [{
-		codigo: 0,
-		pregunta: "¿Cuál es tu lenguaje de programación preferido?",
-		respuestas: ["Javascript", "Java", "C#"]
-	},
-	{
-		codigo: 1,
-		pregunta: "¿Cuál es tu sistema operativo preferido?",
-		respuestas: ["Windows", "Linux", "Solaris"]
-	},
-	{
-		codigo: 2,
-		pregunta: "¿Qué editor de texto usás?",
-		respuestas: ["Sublime", "Visual Studio", "Atom"]
-	},
-	{
-		codigo: 3,
-		pregunta: "¿Cuál fue el primer lenguaje de programación que aprendiste?",
-		respuestas: ["Javascript", "Java", "C#"]
-	}
-];
-
-//Función para crear el select de objetos
-
-function crearSelect() {
-	var selectPaises = `<label for="paises" class="label-paises">País de residencia</label><select name="paises" id="paises"><option value="0">Seleccione una opción</option></select><p id="errorPais">`;
-	$('#datosForm').append(selectPaises);
-};
 //Variables
-
 var arrayResultados;
 var paisesJSON = {
-	"paises": [{
-			"nombre": "Argentina",
-			"codigo": "AR"
-		},
-		{
-			"nombre": "Bolivia",
-			"codigo": "BO"
-		},
-		{
-			"nombre": "Brasil",
-			"codigo": "BR"
-		},
-		{
-			"nombre": "Chile",
-			"codigo": "CL"
-		},
-		{
-			"nombre": "Paraguay",
-			"codigo": "PY"
-		},
-		{
-			"nombre": "Uruguay",
-			"codigo": "UY"
-		},
-	]
+"paises":[
+    {"nombre":"Argentina", "codigo":"AR"},
+    {"nombre":"Bolivia", "codigo":"BO"}, 
+    {"nombre":"Brasil", "codigo":"BR"},
+    {"nombre":"Chile", "codigo":"CL"},
+    {"nombre":"Paraguay", "codigo":"PY"},
+    {"nombre":"Uruguay", "codigo":"UY"},
+]
 };
 
 var paisesArray = paisesJSON.paises;
+var preguntas = [
+{	
+	codigo:0,
+	pregunta:"¿Cuál es tu lenguaje de programación preferido?",
+	respuestas:["Javascript","Java","C#"]
+},
+{	
+	codigo:1,
+	pregunta:"¿Cuál es tu sistema operativo preferido?",
+	respuestas:["Windows","Linux","Solaris"]
+},
+{	
+	codigo:2,
+	pregunta:"¿Qué editor de texto usás?",
+	respuestas:["Sublime","Visual Studio","Atom"]
+},
+{	
+	codigo:3,
+	pregunta:"¿Cuál fue el primer lenguaje de programación que aprendiste?",
+	respuestas:["Javascript","Java","C#"]
+}];
 
-//Función que crea las preguntas
-
-function crearPreguntas(opciones) {
-	$.each(opciones, function (index, elem) {
-		var divPregunta = `<div id="pregunta${index}" class="divPregunta">
-								<label for="enunciado${index}" class="label-enunciado">${elem.pregunta}</label>
-								</div>
-								<p id="error${index}">`
-		$("#datosForm").append(divPregunta)
-		var codigo = index;
-		$.each(elem.respuestas, function (index, elem) {
-			var radioRespuesta = `<input type="radio" name=${codigo} class="option-input radio" value="${elem}"><label>${elem}</label>`
-			var idDiv = "pregunta" + codigo;
-			$('#' + idDiv).append(radioRespuesta);
-		})
-	});
-};
-var preguntas = [{
-		codigo: 0,
-		pregunta: "¿Cuál es tu lenguaje de programación preferido?",
-		respuestas: ["Javascript", "Java", "C#"]
-	},
-	{
-		codigo: 1,
-		pregunta: "¿Cuál es tu sistema operativo preferido?",
-		respuestas: ["Windows", "Linux", "Solaris"]
-	},
-	{
-		codigo: 2,
-		pregunta: "¿Qué editor de texto usás?",
-		respuestas: ["Sublime", "Visual Studio", "Atom"]
-	},
-	{
-		codigo: 3,
-		pregunta: "¿Cuál fue el primer lenguaje de programación que aprendiste?",
-		respuestas: ["Javascript", "Java", "C#"]
-	}
-];
-
-/*AGREGADA*/ /////////
-function recuperarResultados() {
+function recuperarResultados(){
 	var resultadosRecuperados = localStorage.getItem("datosRecopilados");
-	if (resultadosRecuperados === null) {
-		arrayResultados = [];
-	} else {
-		datosParseados = JSON.parse(resultadosRecuperados);
-		arrayResultados = datosParseados.resultados;
+	if(resultadosRecuperados===null) {
+  		arrayResultados=[];
+	}else{
+  	datosParseados = JSON.parse(resultadosRecuperados);
+  	arrayResultados = datosParseados.resultados;
 	}
-	console.log(arrayResultados)
+console.log(arrayResultados)
 }
-/*AGREGADA*/ /////////
 
 //Función para crear el select de objetos
 
-function crearSelect() {
-	var selectPaises = `<label for="paises">País de residencia</label><select name="paises" id="paises"><option value="0">Seleccione una opción</option></select><p id="errorPais">`;
+function crearSelect(){
+	var selectPaises = `<label for="paises" class="label-paises">País de residencia</label><select name="paises" id="paises"><option value="0">Seleccione una opción</option></select><p id="errorPais">`;
 	$('#datosForm').append(selectPaises);
 };
 
 //Función para crear las opciones de país recorriendo el array
 
-function crearOpciones(opciones) {
-	$.each(opciones, function (index, elem) {
+function crearOpciones(opciones){
+	$.each(opciones,function(index,elem){
 		var option = `<option value="${elem.codigo}">${elem.nombre}</option>`
 		$('#paises').append(option);
 	});
@@ -134,81 +63,81 @@ function crearOpciones(opciones) {
 
 //Función que crea las preguntas
 
-function crearPreguntas(opciones) {
-	$.each(opciones, function (index, elem) {
-		var divPregunta = `<div id="pregunta${index}" class="divPregunta">
-							<label for="enunciado${index}">${elem.pregunta}</label>
+function crearPreguntas(opciones){
+	$.each(opciones,function(index,elem){
+		var divPregunta = 	`<div id="pregunta${index}" class="divPregunta">
+							<label for="enunciado${index}" class="label-enunciado">${elem.pregunta}</label>
 							</div>
 							<p id="error${index}">`
 		$("#datosForm").append(divPregunta)
 		var codigo = index;
-		$.each(elem.respuestas, function (index, elem) {
-			var radioRespuesta = `<input type="radio" name=${codigo} value="${elem}"><label>${elem}</label>`
-			var idDiv = "pregunta" + codigo;
-			$('#' + idDiv).append(radioRespuesta);
+		$.each(elem.respuestas, function(index,elem){
+			var radioRespuesta = `<input type="radio" name=${codigo} class="option-input radio" value="${elem}"><label>${elem}</label>`
+			var idDiv = "pregunta"+codigo;
+			$('#'+idDiv).append(radioRespuesta);
 		})
 	});
 };
 
 //Validación del formulario
 
-function validar(event) {
+function validar(event){
 	var valido = true;
 	event.preventDefault();
 	//validación del select
 	var nacionalidad = $('#datosForm :selected').val();
 	//console.log(nacionalidad);
-	if (nacionalidad === "0") {
+	if(nacionalidad === "0"){
 		$('#errorPais').text("Debe seleccionar un país");
 		valido = false;
-	} else {
+	}else{
 		$('#errorPais').text("");
 	}
 
 	//chequear radio buttons
 	var traerPreguntas = $('.divPregunta')
-	for (i = 0; i < preguntas.length; i++) {
-		var opcionSeleccionada = $('input:radio[name=' + i + ']:checked').val();
-		if (!opcionSeleccionada) {
-			valido = false;
-			$('#error' + i).text('Debe seleccionar una opción');
-		} else {
-			$('#error' + i).text('');
+	for(i=0;i<preguntas.length;i++){
+		var opcionSeleccionada = $('input:radio[name='+i+']:checked').val();
+		if(!opcionSeleccionada){
+			valido=false;
+			$('#error'+i).text('Debe seleccionar una opción');
+		}else{
+			$('#error'+i).text('');
 		}
 	};
 
-	if (valido === false) {
+	if(valido===false){
 		return
-	} else {
+	}else{
 		//$("form").submit();
 		recopilarDatos();
 	}
 }
 
-function recopilarDatos() {
 
+function recopilarDatos(){
 	var resultados = {};
 	resultados.pais = $('#datosForm :selected').text();
 	console.log(resultados.pais)
-	for (i = 0; i < preguntas.length; i++) {
-		var opcionResultado = $('input:radio[name=' + i + ']:checked').val();
+	for(i=0;i<preguntas.length;i++){
+		var opcionResultado = $('input:radio[name='+i+']:checked').val();
 		resultados[i] = opcionResultado;
 	}
 
-	//AGREGADA///////////////
 	arrayResultados.push(resultados)
 	var objetoResultados = {
-		"resultados": arrayResultados,
-		"extension": arrayResultados.length
+		"resultados":arrayResultados,
+		"extension":arrayResultados.length
 	}
 	var resultadosJSON = JSON.stringify(objetoResultados);
 	console.log(resultadosJSON)
-	localStorage.setItem("datosRecopilados", resultadosJSON);
-	//AGREGADA///////////////	
+	localStorage.setItem("datosRecopilados",resultadosJSON);
+
 }
-//Llamadas a funciones	
-recuperarResultados();
-crearSelect();
-crearOpciones(paisesArray);
-crearPreguntas(preguntas);
-$('button').on('click', validar);
+
+	//Llamadas a funciones	
+	crearSelect();
+	crearOpciones(paisesArray);
+	crearPreguntas(preguntas);
+	recuperarResultados();
+	$('button').on('click',validar);
